e11bb017f3c641e25629e7bd6e0e590c1973781d
iivchenko <iivchenko@live.com>
2020-08-07 20:13:45 +0200

    Add dummy unit tests

---
+++ src/SaleStatistics.Application.Tests/Queries/GetTopSaleObjects/GetTopSaleObjectsQueryHandlerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace SaleStatistics.Application.Tests.Queries.GetTopSaleObjects
+{
+    [TestFixture]
+    public sealed class GetTopSaleObjectsQueryHandlerTests
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
